f758e84f3ad3aabbb631ac6fffcc135dd834667c
jslee <102969527+eggmong@users.noreply.github.com>
2024-03-05 12:32:49 +0900

    AttackHitCheck AnimNotify + GA 생성, AT_Trace 생성
    5강 0~16:30 / 애니메이션 몽타주의 노티파이를 활용하여 원하는 타이밍에 공격 판정, 애니메이션 노티파이가 발동되면, 판정을 위한 GA를 트리거해 발동

---
+++ Source/ArenaBattleGAS/GA/ABGA_AttackHitCheck.cpp
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GA/ABGA_AttackHitCheck.h"
+#include "ArenaBattleGAS.h"
+
+UABGA_AttackHitCheck::UABGA_AttackHitCheck()
+{
+	InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
+}
+
+void UABGA_AttackHitCheck::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+	ABGAS_LOG(LogABGAS, Log, TEXT("UABGA_AttackHitCheck Begin"));
+
+	bool bReplicatedEndAbility = true;
+	bool bWasCancelled = true;
+	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, bReplicatedEndAbility, bWasCancelled);
+}
---
+++ Source/ArenaBattleGAS/GA/AT/ABAT_Trace.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/Tasks/AbilityTask.h"
+#include "ABAT_Trace.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARENABATTLEGAS_API UABAT_Trace : public UAbilityTask
+{
+	GENERATED_BODY()
+	
+	
+};
